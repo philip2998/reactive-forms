783f749013da3c2963d513b5beb3f83d4cdd9e4d
Philip Gevorgyan <filipgevorgyan@gmail.com>
2023-11-28 14:45:11 +0400

    Fix childForm type error. Change background color

--- src/app/user-contact/user-contact.component.ts
+++ src/app/user-contact/user-contact.component.ts
@@ -8,7 +8,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class UserContactComponent {
   @Input()
-  public childForm: FormGroup;
+  public childForm: any;
 
   @Input()
   public arrayIndex: number;
